22bf3ccdf3fc069518891dd431b59ce7858f01dd
Jonathan Dwyer <jonathan.c.dwyer.mil@army.mil>
2024-11-25 14:07:46 -0600

    initial push

---
+++ src/main/java/com/example/JonathanDwyer/JonathanDwyerApplication.java
@@ -0,0 +1,13 @@
+package com.example.JonathanDwyer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JonathanDwyerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JonathanDwyerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JonathanDwyer/JonathanDwyerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JonathanDwyer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JonathanDwyerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
